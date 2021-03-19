e6fa7f21a3f8ddb710043a48d21515c68a5df7bc
Patrick Corcoran <patrickcorcoran@Patricks-MacBook-Pro.local>
2021-03-19 12:10:10 -0500

    new commit

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 
 require("./routes/apiRoutes")(app)
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
